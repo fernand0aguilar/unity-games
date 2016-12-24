1abd08b4f7e12f2339a055965f13c1287f1ec692
Fernando <fernando@aguilar.net.br>
2016-12-24 20:28:14 -0200

    Adding story continuation and key on book logic

--- Text101/Assets/TextController.cs
+++ Text101/Assets/TextController.cs
@@ -5,9 +5,10 @@ using UnityEngine.UI;
 public class TextController : MonoBehaviour {
 	
 	public Text myText;
-	public enum Status {locked_cell, bed, b_sleep, window, mirror, library, l_guard, l_book, l_key, l_help_guard, freedom};
+	public enum Status {locked_cell, bed, b_sleep, b_sleep_1, window, mirror, library, l_guard, l_book, l_key, l_help_guard, freedom};
 	
 	public Status currentStatus;
+	public bool key_on_book = false;
 	
 	// Use this for initialization
 	void Start () {
@@ -25,6 +26,9 @@ public class TextController : MonoBehaviour {
 			case Status.b_sleep:
 				state_bed_sleep_action();
 				break;
+			case Status.b_sleep_1:
+				state_bed_sleep_1_action();
+				break;
 			case Status.window:
 				state_window_action();
 				break;
@@ -41,7 +45,7 @@ public class TextController : MonoBehaviour {
 				state_key_action();
 				break;
 			case Status.l_guard:
-				state_guard_action();
+				state_help_guard_action();
 				break;
 		}
 	}
@@ -50,7 +54,7 @@ public class TextController : MonoBehaviour {
 		currentStatus = Status.locked_cell;			  
 		
 		myText.text = "You are locked in your cell. Reading and thinking about freedom are the only thing that keeps you alive.\n" +
-					  "After a couple of years, you and the guards have quite a good relashionship.\n" +
+					  "After a couple of years, the guards actually respect you.\n\n" +
 					  "Press 'B' to lay in bed.\n" +
 					  "'W' to look trough the window.\n" + 
 					  "'M' to look at the mirror.\n" +
@@ -67,8 +71,8 @@ public class TextController : MonoBehaviour {
 	}
 	
 	void state_bed_action(){
-		myText.text = "Your sheets are disgusting. How can a human beeing sleep in this?\n" +
-					  "You feel tired. And decide to lay even if the sheets are dirtier than the deans moral.\n\n" +
+		myText.text = "Your sheets are disgusting. \nHow can a human beeing sleep in this!?\n" +
+					  "You feel tired. And decide to lay down.\nEven if the sheets are dirtier than the deans moral.\n\n" +
 					  "Press 'S' to sleep. 'U' to get up again.";
 			
 		if(Input.GetKeyDown(KeyCode.S)) 
@@ -82,10 +86,17 @@ public class TextController : MonoBehaviour {
 					  "Taking a nap is the best option that you have. Even in these sheets. \nSleeping is sleeping." +
 					  "\n\n...6 hours later...\nYou wake up feeling alive.\n Press 'M' to sleep more. 'U' to get up";
 		if(Input.GetKeyDown(KeyCode.M)){ 
-			myText.text = "You close your eyes again for two more hours.\n Your body can't stand one more minute on this bed." +
-						  "You get up.\n\n Press 'U' to continue";
+			currentStatus = Status.b_sleep_1;
 		}
 		else if(Input.GetKeyDown(KeyCode.U)){
+			currentStatus = Status.b_sleep_1;
+		}
+	}
+	void state_bed_sleep_1_action(){
+		myText.text = "You close your eyes again for two more hours.\n Your body can't stand one more minute on this bed." +
+			"You get up.\n\n Press 'U' to continue";
+		
+		if(Input.GetKeyDown(KeyCode.U)){
 			currentStatus = Status.locked_cell;
 		}
 	}
@@ -108,15 +119,20 @@ public class TextController : MonoBehaviour {
 			currentStatus = Status.locked_cell;	
 	}
 	void state_library_action(){
-		myText.text = "After reading 'Thus spoke Zarastrusta', the unique book, you are on the hunt for another great book as it.\n" +
-					  "Since Bruce, the guard, is a ambicious reader, just like you, he decides to take you to the library, in order to find a new book to read" +
+		myText.text = "After reading 'Thus spoke Zarastrusta', the unique book as it is, you request another great book.\n\n" +
+					  "Since Bruce, the guard, is a ambicious reader, just like you,\n he decides to take you to the library, in order to find a new book to read.\n" +
 					  "It is against the rules. But you are the most calm inmate on there, and the prison also has many other guards. It is impossible to break trough." +
 					  "\n\nPress ENTER to continue";
 		if(Input.GetKeyDown(KeyCode.Return))
 			currentStatus = Status.l_book;
 	}
-	void state_library_action(){
-		myText.text = "TODO";
+	
+	void state_book_action(){
+		myText.text = "You two get at the library and are talking about books and authors.\nIn a moment of pure 'what the fuck'," +
+			"Bruce fall on the ground. Completly blacked out. You think what the hell just happened, but then think also on the opportunities.\n" +
+			"Since he just felt down blacked out, you have a few moments alone with his body, and with his tools... It is time to act and be free.\n" +
+			"\nBut you are smart and plans the future.\n The keys are on his pocket.\n\n" +
+			"Press 'G' to call for help. 'K' to Pick up the set of keys";
 		
 		if(Input.GetKeyDown(KeyCode.G))
 			currentStatus = Status.l_help_guard;
@@ -124,7 +140,36 @@ public class TextController : MonoBehaviour {
 			currentStatus = Status.l_key;
 	}
 	
-	//TODO Finish library actions, library keys, library help guard, and hide key on book;
+	void state_key_action(){
+		key_on_book = true;
+		
+		myText.text = "In a moment of pure higher concsiousness, you pick only your cell number key out of the set of keys in his pocket.\n" +
+			"Hiding it on the book called 'Mathematics for experts', using his knife to crave a hole, inserting in it.\n"+
+			"You put the book on the shelf, and call for help" +
+			"Press 'ENTER' to continue";
+		
+		if(Input.GetKeyDown(KeyCode.Return)){
+			currentStatus = Status.l_help_guard;
+		}	
+	}
+	
+	void state_help_guard_action(){
+		if (key_on_book == true){
+			myText.text = "You have helped the guard. Calling other guards and explaning the situation."+
+				"By some sort of miracle, he is okay and did not noticed the key missing.\n" +
+				"You now have the oportunity to escape. Just need to get the book wich the key is inside.\n" +
+				"Press ENTER to return to your cell and think about getting the book"; 
+		}
+		
+		else{
+			myText.text = "You have helped the guard, and after much explanations, your routine go back as normal.\nYou failed to ESCAPE by making good moral choices"+
+				"\n\n Press ENTER to return to your cell";
+		}
+		
+		if(Input.GetKeyDown(KeyCode.Return)){
+			currentStatus = Status.locked_cell;
+		}
+	}
 	
 	void state_freedom(){
 		myText.text = "";
