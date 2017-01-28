249fbc734ee88cd4f2d51b8f5a6dd611efbc6820
Fernando <fernando@aguilar.net.br>
2017-01-28 20:18:17 -0200

    Including paddle, ball and discrete collision

--- Brick Breaker/Assets/Scripts/LooseCollider.cs
+++ Brick Breaker/Assets/Scripts/LooseCollider.cs
@@ -3,8 +3,11 @@ using System.Collections;
 
 public class LooseCollider : MonoBehaviour {
 
+	public LevelMananger mananger;
+	
 	void OnTriggerEnter2D (Collider2D collider){
 		print ("Passou");
+		mananger.LoadLevel("Win");
 	}
 	
 	void OnCollisionEnter2D (Collision2D collision){
