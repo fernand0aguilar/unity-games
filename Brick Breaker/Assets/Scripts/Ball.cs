4ab9018b15633828a54f5149d0be55bc1f2eea8b
Fernando <fernando@aguilar.net.br>
2017-01-28 23:33:06 -0200

    Launching ball and keeping trak of it on paddle

---
+++ Brick Breaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+	
+	public Paddle paddle;
+	
+	private bool hasStarted = false;
+	private Vector3 paddleToBallVector;
+	
+	// Use this for initialization
+	void Start () {
+		paddleToBallVector = this.transform.position - paddle.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!hasStarted){
+			//Lock ball relative to paddle
+			this.transform.position = paddle.transform.position + paddleToBallVector;
+			
+			//Wait for mouse press to launch
+			if(Input.GetMouseButtonDown(0)){
+				hasStarted = true;
+				
+				//Release ball from paddle
+				this.rigidbody2D.velocity = new Vector2(2f, 10f);
+			}
+		}
+	}
+}
---
+++ Brick Breaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paddle : MonoBehaviour {
+
+	void start(){
+	
+	}
+
+	void Update () {
+	
+		Vector3 paddlePosition;
+		float mousePosInBlocks;
+		
+		paddlePosition = new Vector3(0.5f, this.transform.position.y, 0f);
+		
+		mousePosInBlocks = Input.mousePosition.x / Screen.width * 8;
+	
+		// Set limits to the paddle inside screen (min 0.5 , max 7.5);
+		
+		paddlePosition.x = Mathf.Clamp(mousePosInBlocks, 0.5f, 7.5f);
+		
+		this.transform.position = paddlePosition;
+		
+		
+	}
+	
+}
